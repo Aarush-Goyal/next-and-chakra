b78601a6787eb8fadac95e82cdea6349b7fbf4c8
Aarush-Goyal <goyalaarush92@gmail.com>
2021-06-20 10:48:39 +0530

    added: ( protected routes )

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,11 +1,33 @@
+import React, { useEffect, useState } from 'react'
 import Head from 'next/head'
-import React from 'react'
 import { project } from '../config/project'
 import Header from '../components/Header'
-import { Stack, Container, Heading, Box, Text, Grid, GridItem } from '@chakra-ui/layout'
-import { Image } from '@chakra-ui/image'
+import { Grid, GridItem } from '@chakra-ui/layout'
+import { useSession } from 'next-auth/client'
+import AccessDenied from '../components/AccessDenied'
 
-const Dashboard = () => {
+
+export default function Page() {
+    const [session, loading] = useSession()
+    const [content, setContent] = useState()
+
+    // Fetch content from protected route
+    useEffect(() => {
+        const fetchData = async () => {
+            const res = await fetch('/api/protected')
+            const json = await res.json()
+            if (json.content) { setContent(json.content) }
+        }
+        fetchData()
+    }, [session])
+
+    // When rendering client side don't display anything until loading is complete
+    if (typeof window !== 'undefined' && loading) return null
+
+    // If no session exists, display access denied message
+    if (!session) { return <AccessDenied /> }
+
+    // If session exists, display content
     return (
         <>
             <Head>
@@ -31,4 +53,3 @@ const Dashboard = () => {
     )
 }
 
-export default Dashboard
